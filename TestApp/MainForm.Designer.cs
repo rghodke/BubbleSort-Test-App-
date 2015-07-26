e16ac921a7e3d064e8f617a731f2041065b0ef61
Ranjit Ghodke <rghodke@ucsd.edu>
2015-07-25 19:04:39 -0700

    BubbleSort App
    Written in C#
    Utilizes
    - Windows Form
    - SQL
    - .Net

---
+++ TestApp/MainForm.Designer.cs
@@ -0,0 +1,240 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Admin
+ * Date: 7/21/2015
+ * Time: 6:56 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace TestApp
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button ShuffleButton;
+		private System.Windows.Forms.Button StepButton;
+		private System.Windows.Forms.Button PlayButton;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.ShuffleButton = new System.Windows.Forms.Button();
+			this.StepButton = new System.Windows.Forms.Button();
+			this.PlayButton = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// ShuffleButton
+			// 
+			this.ShuffleButton.Location = new System.Drawing.Point(497, 12);
+			this.ShuffleButton.Name = "ShuffleButton";
+			this.ShuffleButton.Size = new System.Drawing.Size(75, 23);
+			this.ShuffleButton.TabIndex = 0;
+			this.ShuffleButton.Text = "Shuffle";
+			this.ShuffleButton.UseVisualStyleBackColor = true;
+			this.ShuffleButton.Click += new System.EventHandler(this.ShuffleButtonClick);
+			// 
+			// StepButton
+			// 
+			this.StepButton.Location = new System.Drawing.Point(497, 41);
+			this.StepButton.Name = "StepButton";
+			this.StepButton.Size = new System.Drawing.Size(75, 23);
+			this.StepButton.TabIndex = 1;
+			this.StepButton.Text = "Step";
+			this.StepButton.UseVisualStyleBackColor = true;
+			this.StepButton.Click += new System.EventHandler(this.StepButtonClick);
+			// 
+			// PlayButton
+			// 
+			this.PlayButton.Location = new System.Drawing.Point(497, 71);
+			this.PlayButton.Name = "PlayButton";
+			this.PlayButton.Size = new System.Drawing.Size(75, 23);
+			this.PlayButton.TabIndex = 2;
+			this.PlayButton.Text = "Play";
+			this.PlayButton.UseVisualStyleBackColor = true;
+			this.PlayButton.Click += new System.EventHandler(this.PlayButtonClick);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(106, 14);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(106, 42);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 4;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(106, 72);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 5;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(368, 15);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 6;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(368, 43);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 7;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(325, 72);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(143, 20);
+			this.textBox6.TabIndex = 8;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(60, 17);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 19);
+			this.label1.TabIndex = 9;
+			this.label1.Text = "Index";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(60, 45);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(41, 20);
+			this.label2.TabIndex = 10;
+			this.label2.Text = "Current";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(61, 74);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(40, 20);
+			this.label3.TabIndex = 11;
+			this.label3.Text = "Next";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(325, 17);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(37, 14);
+			this.label4.TabIndex = 12;
+			this.label4.Text = "Swap";
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(308, 46);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(54, 16);
+			this.label5.TabIndex = 13;
+			this.label5.Text = "Swapped";
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.AllowUserToResizeColumns = false;
+			this.dataGridView1.AllowUserToResizeRows = false;
+			this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+			this.dataGridView1.Enabled = false;
+			this.dataGridView1.Location = new System.Drawing.Point(1, 99);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersVisible = false;
+			this.dataGridView1.Size = new System.Drawing.Size(205, 243);
+			this.dataGridView1.TabIndex = 17;
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(244, 209);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(328, 23);
+			this.progressBar1.TabIndex = 18;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(584, 361);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.PlayButton);
+			this.Controls.Add(this.StepButton);
+			this.Controls.Add(this.ShuffleButton);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "MainForm";
+			this.Text = "TestApp";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ TestApp/MainForm.cs
@@ -0,0 +1,247 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Admin
+ * Date: 7/21/2015
+ * Time: 6:56 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TestApp
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		ArrayList points = new ArrayList();
+		int curloc = 0;
+		int curmax = 9;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			dataGridView1.ColumnCount = 2;
+			dataGridView1.Columns[0].Name = "Index";
+			dataGridView1.Columns[1].Name = "Value";
+			
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+			progressBar1.Maximum = 450;
+			Shuffle();
+			HighlightRows();
+			UpdateFields();
+			
+			//SQL STUFF
+			SqlConnection tempConnection = new SqlConnection("user id=AdminTest;" +
+			                                                 "password=AdminTest; server=RON\\SQLEXPRESS;" +
+			                                                 "Trusted_Connection=yes;" +
+			                                                 "database=AdminTest; " +
+			                                                 "connection timeout=5");
+			try
+			{
+				tempConnection.Open();
+			}
+			catch(Exception e)
+			{
+				
+			}
+			
+			SqlCommand  myCommand = new SqlCommand("Command String", tempConnection);
+			SqlCommand myCommand2 = new SqlCommand("INSERT INTO table1 (column1, column2) " +
+			                                       "Values ('string', 1)", tempConnection);
+			myCommand2.ExecuteNonQuery();
+			
+			try
+			{
+				SqlDataReader myReader = null;
+				SqlCommand    myCommand3 = new SqlCommand("select * from table1",
+				                                          tempConnection);
+				myReader = myCommand3.ExecuteReader();
+				while(myReader.Read())
+				{
+
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+
+			SqlCommand myCommand4 = new SqlCommand(
+				"SELECT * FROM table1 WHERE Column = " + " ", tempConnection);
+			
+			SqlParameter myParam = new SqlParameter("@Param1", SqlDbType.VarChar, 11);
+			myParam.Value = "Garden Hose";
+			
+			SqlParameter myParam2 = new SqlParameter("@Param2", SqlDbType.Int, 4);
+			myParam2.Value = 42;
+
+			SqlParameter myParam3 = new SqlParameter("@Param3", SqlDbType.Text);
+			myParam.Value = "Note that I am not specifying size. " +
+				"If I did that it would trunicate the text.";
+			
+			SqlCommand myCommand5 = new SqlCommand(
+				"SELECT * FROM table WHERE Column = @Param2", tempConnection);
+			myCommand.Parameters.Add(myParam2);
+			
+			try
+			{
+				tempConnection.Close();
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+			
+			
+			//END SQL STUFF
+			
+		}
+		
+		void Shuffle()
+		{
+			timer.Enabled = false;
+			StepButton.Enabled = true;
+			PlayButton.Enabled = true;
+			textBox4.Text = "";
+			textBox5.Text = "";
+			textBox6.Text = "";
+			points = new ArrayList();
+			dataGridView1.Rows.Clear();
+			Random random = new Random();
+			
+			for(int i = 0; i<10; i++)
+			{
+				points.Add(random.Next(0, 100));
+				dataGridView1.Rows.Add(i.ToString(), points[i]) ;
+			}
+			curloc = 0;
+			curmax = 9;
+			progressBar1.Value = 0;
+			UpdateFields();
+			HighlightRows();
+
+			StepButton.Enabled = true;
+			
+			
+		}
+		
+		void UpdateProgress()
+		{
+			dataGridView1.Rows.Clear();
+			for(int i = 0; i<10; i++)
+			{
+				dataGridView1.Rows.Add(i.ToString(), points[i]) ;
+			}
+			progressBar1.Step = 10;
+			progressBar1.PerformStep();
+			
+			
+		}
+		
+		void ShuffleButtonClick(object sender, EventArgs e)
+		{
+			Shuffle();
+
+		}
+
+		
+
+		void ProgressSort()
+		{
+			if ( curloc < curmax )
+			{
+				string temp1 = (points[curloc + 1]).ToString();
+				string temp2 = (points[curloc]).ToString();
+				if(Int32.Parse(temp1) > Int32.Parse(temp2))
+				{
+					textBox4.Text = points[curloc].ToString();
+					textBox5.Text = "true";
+					textBox6.Text = temp2 + " has swapped with " + temp1;
+					points.Reverse(curloc, 2);
+				}
+				else
+				{
+					textBox4.Text = "";
+					textBox5.Text = "false";
+					textBox6.Text = temp2 + " did not swap with " + temp1;
+				}
+				curloc++;
+			}
+			UpdateProgress();
+			if ( curloc == curmax )
+			{
+				curloc = 0;
+				curmax--;
+			}
+			if(curmax == 0)
+			{
+				StepButton.Enabled = false;
+				PlayButton.Enabled = false;
+				ShuffleButton.Enabled = true;
+			}
+			
+			UpdateFields();
+			
+		}
+		
+
+		void StepButtonClick(object sender, EventArgs e)
+		{
+			ProgressSort();
+			HighlightRows();
+
+		}
+		
+		void HighlightRows()
+		{
+			DataGridViewRow tempr0 = dataGridView1.Rows[curloc];
+			DataGridViewRow tempr1 = dataGridView1.Rows[curloc+1];
+			tempr0.DefaultCellStyle.BackColor = Color.Tomato;
+			tempr1.DefaultCellStyle.BackColor = Color.Tomato;
+		}
+		
+		void UpdateFields()
+		{
+			textBox1.Text = curloc.ToString();
+			textBox2.Text = points[curloc].ToString();
+			textBox3.Text = points[curloc+1].ToString();
+		}
+
+		Timer timer = new Timer();
+		
+		void PlayButtonClick(object sender, EventArgs e)
+		{
+			timer = new Timer();
+			timer.Tick += new EventHandler(timer_Tick);
+			timer.Interval = (1000) * (1);
+			timer.Enabled = true;
+			timer.Start();
+			PlayButton.Enabled = false;
+			ShuffleButton.Enabled = false;
+			
+			
+		}
+		
+		void timer_Tick(object sender, EventArgs e)
+		{
+			ProgressSort();
+			HighlightRows();
+
+		}
+		
+		
+
+
+	}
+}
